a1536033a4c510efbfb4d7f7fce6c37665fe05d3
SATO Daisuke <densuke@fuga.jp>
2020-08-24 16:40:21 +0900

    ベクタ版

--- src/bin/no035.rs
+++ src/bin/no035.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let array = [3, 7, 0, 8, 4, 1, 9, 6, 5, 2];
-    let number = mylib::get_number_with_prompt("input number: ") as usize;
-    println!("array[{}] = {}", number, array[number]);
+    let array = vec![3u8, 7, 0, 8, 4, 1, 9, 6, 5, 2];
+    let number = mylib::get_number_with_prompt("input number: ");
+    println!("array[{}] = {}", number, array.get(number as usize).unwrap());
 
 }
